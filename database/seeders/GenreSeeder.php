c78172b264dc5b4579f25cdf03f847dcbba779bc
vsantidev <vsantidev@gmail.com>
2023-11-28 11:23:16 +0100

    mise en place des genre et des tags + preparation BookFactory

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use function PHPSTORM_META\type;
+
 class GenreSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
+    private $categories = ['BD', 'Manga', 'Policier', 'Sciences Fiction', 'Narratif', 'Poetique', 'Theatral', 'Fantastique', 'Thriller', 'Jeunesse', 'Biographies', 'Littérature', 'Romance'];
+    
     public function run(): void
     {
-        //
+        foreach ($this->categories as $genre) {
+            Genre::factory()->create([
+            'name'=> $genre]);
+        }
     }
 }

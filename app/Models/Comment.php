5d509183c2df5155655296fa06df61f75ed84ca2
sergioMG98 <sergio.marquesgomes98@gmail.com>
2023-11-30 16:55:50 +0100

    probleme de models rectifier

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 'comment', 'note', 'user_id' ];
+
+    //premet d'eviter l'envoi de la date et l'heure
+    public $timestamps = false;
+
+}

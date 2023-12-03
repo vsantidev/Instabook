8c8cbec3b1b062dac913fe6e62ad6b7afd73e8a6
vsantidev <vsantidev@gmail.com>
2023-12-03 11:38:19 +0100

    refonte de la page create + qqs petites modif nav

--- resources/views/bookview/create.blade.php
+++ resources/views/bookview/create.blade.php
@@ -11,36 +11,42 @@
 
 {{-- SECTION FORMULAIRE START--}}
 <section class="creation">
-    <h1>Création du livre</h1>
-    <div class="box-container">
-        <div class="box">
-            <form action='{{route('book.store')}}' method='post' class="contentList__card" enctype="multipart/form-data">
-                @csrf
-                <input type='text' name='title' placeholder="Titre du livre" class="border-gray-600 border-2 block my-2">
-                <input type='year' name='annee' placeholder="Anne du livre" class="border-gray-600 border-2 block my-2">
-        
-                <textarea type='text' name='synopsis' cols="30" rows="10" placeholder="Résumé du livre"></textarea>
-                *maximum 90 caractères
-                <select type='text' name='author' placeholder="Auteur / Autrice du livre">
-                    <option value="0">Nouveau</option>
+    <h1 class="title">Création du livre</h1>
+    <div class="row">
+
+        <form action='{{route('book.store')}}' method='post' class="content" enctype="multipart/form-data">
+            @csrf
+            <input type='text' name='title' placeholder="Titre du livre" class="box">
+            <input type='year' name='annee' placeholder="Année du livre" class="box">
+            <p>Le résumé doit-être compris entre 10 et 120 caractères</p>
+            <textarea type='text' name='synopsis' cols="30" rows="10" placeholder="Résumé du livre" class="textaera"></textarea>
+            
+            <div class="box">
+                <label for="avatar" class="picture">Renseignez l'auteur ou l'autrice :</label>
+                <p>Si le nom n'a pas était encore enregistré, veuillez laisser l'onglet sur "Nouveau" et rentrée le nom et le prénom dans les lignes ci-dessous!</p>
+                <select type='text' name='author' placeholder="Auteur / Autrice du livre" class="option">
+                    <option value="0"> -- Nouveau --</option>
                     @foreach ($authors as $author)
                         <option value="{{$author->id}}">{{$author->lastname}} {{$author->firstname}}</option>
                     @endforeach
                 </select>
-                <input type="text" name="firstname" placeholder="Nom">
-                <input type="text" name="lastname" placeholder="Prénom">
-                
-                {{-- <input type='number' name='note' placeholder="Nombre de follower"> --}}
-                <label for="avatar">Choississez la couverture du livre:</label>
-                <input type="file" id="image" name="image" accept="image/png, image/jpeg" />
-                <select name="genre">
+                <input type="text" name="firstname" placeholder="Nom" class="box">
+                <input type="text" name="lastname" placeholder="Prénom" class="box">
+            </div>
+
+            <div class="box">
+                <label for="avatar" class="picture">Choississez la couverture du livre :</label>
+                <input type="file" id="image" name="image" accept="image/png, image/jpeg" class="file" />
+                <select name="genre" class="option">
+                    <option value="0"> -- Genre --</option>
                     @foreach ($arrayGenre as $item)
+                    
                         <option value="{{$item->id}}">{{$item->name}}</option>
                     @endforeach
                 </select>
-                <button type='submit' class="button" >Créer</button>
-            </form>
-        </div>
+            </div>
+            <button type='submit' class="button" >Créer</button>
+        </form>
     </div>
 </section>
 
